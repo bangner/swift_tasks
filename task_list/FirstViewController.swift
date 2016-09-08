2a07ec0ee5afd8df17dbd1cc77a6e932f34c6394
Joe Wagner <joe@mobilexlabs.com>
2016-09-07 21:56:40 -0500

    Initial Commit

---
+++ task_list/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  task_list
+//
+//  Created by Joe Wagner on 9/7/16.
+//  Copyright © 2016 Bangner. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
